476849c702b2831bff8f2a4ef5bdabee898a50dc
Roman <tu_email@example.com>
2025-11-13 18:54:46 +0100

    fix: eliminar entidades duplicadas en package domain y corregir LevelyApplication duplicado; puerto H2=8081

--- src/main/java/levely/LevelyApplication.java
+++ src/main/java/levely/LevelyApplication.java
@@ -56,14 +56,3 @@ public class LevelyApplication {
         };
     }
 }
-package levely;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class LevelyApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(LevelyApplication.class, args);
-    }
-}
